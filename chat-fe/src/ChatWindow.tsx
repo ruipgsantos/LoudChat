363b0072818979bff7baa406d5e2ef5a0c3c503b
Rui Santos <ruipgs@gmail.com>
2023-05-10 17:56:04 +0100

    client aware of usermessage, session fix

--- chat-fe/src/ChatWindow.tsx
+++ chat-fe/src/ChatWindow.tsx
@@ -2,17 +2,18 @@
 import "./chatwindow.css";
 import { useEffect, useCallback, useState, useRef } from "react";
 import io, { Socket } from "socket.io-client";
+import { UserMessage } from "./external/UserMessage";
+import { CHAT_EVENT, CONNECT_EVENT } from "./external/SocketEvents";
 
-const CHAT_EVENT: string = process.env.REACT_APP_CHAT_EVENT || "chat_event";
 
 export default function ChatWindow() {
-    const [talkText, setTalkText] = useState<string>("");
 
-    const [currentChat, setCurrentChat] = useState("");
+
+    const [talkText, setTalkText] = useState<string>("");
+    const [currentChat, setCurrentChat] = useState<UserMessage[]>([]);
     const chatTextRef = useRef<any>(null);
 
     const [socket, setSocket] = useState<Socket>();
-    const [connected, setConnected] = useState<boolean>(false);
 
     const keepToBottom = useCallback(() => {
         setTimeout(() => {
@@ -22,32 +23,35 @@ export default function ChatWindow() {
 
     //simply send message to server, talk
     const talk = useCallback(() => {
-        if (connected) {
+        if (socket) {
             const phrase = talkText;
             socket?.emit(CHAT_EVENT, phrase);
-            setCurrentChat(cc => `${cc} ${phrase}`);
             setTalkText("");
 
             keepToBottom();
         }
-    }, [connected, keepToBottom, socket, talkText])
+    }, [keepToBottom, socket, talkText])
 
 
     //wait for socket connect
     const setupSocket = useCallback(() => {
         if (socket) return;
-        const socketio = io(process.env.REACT_APP_CHAT_API as string);
+        const socketio = io(process.env.REACT_APP_CHAT_API as string, { withCredentials: true });
         socketio.on("connect", () => {
             setSocket(socketio);
-            setConnected(true);
-        });
-    }, [socket])
 
-    //set chat event
-    useEffect(() => {
-        socket?.on(CHAT_EVENT, (arg) => {
-            setCurrentChat(cc => `${cc} ${arg}`);
-            keepToBottom();
+
+            socketio?.on(CHAT_EVENT, (userMessage: UserMessage) => {
+                setCurrentChat((cc) => {
+                    return [...cc, userMessage]
+                })
+                keepToBottom();
+            });
+
+            socketio?.on(CONNECT_EVENT, (userMessages: UserMessage[]) => {
+                setCurrentChat(userMessages);
+                keepToBottom();
+            })
         });
     }, [keepToBottom, socket])
 
@@ -57,13 +61,26 @@ export default function ChatWindow() {
         //eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    const getMessageElements = () => {
+        const res = [];
+
+        for (let i = 0; i < currentChat.length; i++) {
+            const words = currentChat[i].message.split(' ');
+            for (let j = 0; j < words.length; j++) {
+                res.push(<span key={j * i} style={{ fontSize: currentChat[i].size * 1.5 + 10 }}>{words[j]}</span>)
+            }
+        }
+
+        return res;
+    }
+
 
     return (
         <div className="chatwindow">
             <div className="chatContainer">
-                <textarea ref={chatTextRef} value={currentChat} readOnly
-                >
-                </textarea>
+                <div ref={chatTextRef} className="textArea">
+                    {getMessageElements()}
+                </div>
             </div>
             <div className="inputContainer">
                 <input onKeyDown={(e) => {
---
+++ chat-fe/src/external/SocketEvents.ts
@@ -0,0 +1,4 @@
+const CHAT_EVENT = "chat_event";
+const CONNECT_EVENT = "connect_event";
+
+export { CHAT_EVENT, CONNECT_EVENT };
