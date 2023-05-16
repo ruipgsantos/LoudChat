9dab331f278b03e69d6a36ae976687269bb50b9a
Rui Santos <ruipgs@gmail.com>
2023-05-16 17:14:22 +0100

    testing cors logs

--- chat/src/utils.ts
+++ chat/src/utils.ts
@@ -1,7 +1,11 @@
 export const getCors = (): any[] => {
   const corsArr: string[] = process.env.CORS_ORIGINS?.split(",") || [];
 
+  console.log(`found cors: ${corsArr}`);
+
   const requestedCors = corsArr.map((corsStr) => {
+    console.log(`string: ${corsStr}`);
+
     const isRegex = corsStr.at(0) === "^";
 
     return isRegex ? new RegExp(corsStr) : corsStr;
