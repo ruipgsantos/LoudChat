ef10cc1dd770977147d1927de384d1b79901c1bb
Rui Santos <ruipgs@gmail.com>
2023-05-24 10:35:32 +0100

    env_refactor

--- chat/src/services/redis-chat-cache.service.ts
+++ chat/src/services/redis-chat-cache.service.ts
@@ -24,6 +24,7 @@ export default class RedisChatCache implements CacheService<UserMessage> {
         port: redisConf.port,
       },
       password: redisConf.password,
+      username: redisConf.user,      
     });
 
     this._client.on("error", (err: any) => {
