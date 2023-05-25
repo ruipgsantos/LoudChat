f113aed4a2b7d7b15af81cbfa2c76a3fd469bc6c
Rui Santos <ruipgs@gmail.com>
2023-05-25 15:40:28 +0100

    front end online users info

--- chat-fe/src/external/SocketEvents.ts
+++ chat-fe/src/external/SocketEvents.ts
@@ -1,5 +1,6 @@
 const CHAT_EVENT = "chat_event";
 const CONNECT_EVENT = "connect_event";
 const TICK_EVENT = "tick_event";
+const USER_EVENT = "user_event";
 
-export { CHAT_EVENT, CONNECT_EVENT, TICK_EVENT };
+export { CHAT_EVENT, CONNECT_EVENT, TICK_EVENT, USER_EVENT };
